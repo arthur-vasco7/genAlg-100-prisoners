0fbf2ea948b65dce6a9c9c53254c6249d53ee604
arthur <140714079+arthur-vasco7@users.noreply.github.com>
2025-11-16 22:28:05 -0300

    Update fitness.py

--- src/algorithm/fitness.py
+++ src/algorithm/fitness.py
@@ -1,2 +1,14 @@
 # fintess function para avaliar a aptidão das soluções
+import permutations
+
+def countPrisoners(population: [[int]]) -> float:
+    acc = 0
+    for i in range(len(population)):
+        if (i+1 in population[i]):
+            acc += 1
+
+    return acc/100
+
+if (__name__ == "__main__"):
+    print(countPrisoners(permutations.newGenome()))
 
